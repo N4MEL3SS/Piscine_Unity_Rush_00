4c11d8acb08ba4e36d5a7a9e2224e1a8da8aa5af
celadia <celadia@student.21-school.ru>
2022-06-19 18:01:16 +0300

    add prefabs 2

--- Assets/Scripts/WeaponChangeScript.cs
+++ Assets/Scripts/WeaponChangeScript.cs
@@ -1,7 +1,31 @@
+using System;
 using UnityEngine;
 
 
 public class WeaponChangeScript : MonoBehaviour
 {
-		
+	public string currentWeaponType;
+	public bool inTrigger = false;
+
+	private void Update()
+	{
+		WeaponManager();
+	}
+
+	public void WeaponManager()
+	{
+		if (Input.GetMouseButtonDown(1) &&!inTrigger)
+			DropWeapon(currentWeaponType);
+	}
+
+	public void DropWeapon(string weapon)
+	{
+		if (currentWeaponType != "0-Empty")
+		{
+			Instantiate(Resources.Load("Prefabs/Weapons" + weapon), transform.position, Quaternion.identity);
+			if (!inTrigger)
+				currentWeaponType = "0-Empty";
+		}
+	}
+	
 }
